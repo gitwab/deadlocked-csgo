e8508d710d4eaf2d4e089d0a64bf7abf7e275d77
gitwab <lisetwalid@gmail.com>
2026-09-14 18:08:56 +0200

    Update offsets for CS:GO - add dwLocalPlayer, dwEntityList, and CS:GO-specific entity offsets

--- cheat/src/cs2/offsets.rs
+++ cheat/src/cs2/offsets.rs
@@ -83,6 +83,10 @@ macro_rules! field_stmt {
         let window = $proc.read(window);
         $off.$field.$fdef = $proc.get_relative_address(window, $c, $d);
     }};
+    // CS:GO specific offsets (fixed values)
+    ($proc:ident, $off:ident, $client:ident, $physics:ident, $field:ident, $fdef:ident, csgo_fixed ($value:literal)) => {{
+        $off.$field.$fdef = $value;
+    }};
 }
 
 macro_rules! schema {
@@ -173,12 +177,14 @@ schema! {
     }
 
     direct: DirectOffsets {
-        local_player: scan("48 83 3D ? ? ? ? 00 0F 95 C0 C3", client, rel(0x03, 0x08)),
+        // CS:GO offsets - using fixed offsets provided
+        local_player: csgo_fixed(0xDEA98C),
+        entity_list: csgo_fixed(0x4DDC8F4),
+        view_matrix: csgo_fixed(0x4D0F250),
         button_state: interface_fn(interface, input, 19, 0x14),
-        view_matrix: scan("C6 83 ? ? 00 00 01 4C 8D 05", client, rel(0x0A, 0x00, 0x04)),
         sdl_window: module_export(sdl, "SDL_GetKeyboardFocus", rel(0x02, 0x06, 0x03, 0x07)),
-        global_vars: scan("48 8D 05 ? ? ? ? 45 31 E4 48 8B 00 8B 78 10", client, rel(0x03, 0x07)),
-        vphys_world: scan_ptr("4c 8d 35 ? ? ? ? 49 8b 3e e8 ? ? ? ? 48 89 c2", client, rel(3, 7)),
+        global_vars: scan("48 8D 05 ? ? ? ? 45 31 E4 48 8B 00 8B 78 10", engine, rel(0x03, 0x07)),
+        vphys_world: scan_ptr("4c 8d 35 ? ? ? ? 49 8b 3e e8 ? ? ? ? 48 89 c2", engine, rel(3, 7)),
         build_date: scan("4c 89 e6 e8 ? ? ? ? 48 8d 35 ? ? ? ? 48 8d 3d", engine, rel(11, 15)),
     }
 
@@ -189,132 +195,131 @@ schema! {
 
     client: {
         controller: PlayerControllerOffsets {
-            steam_id: schema("CBasePlayerController", "m_steamID"),
-            money_services: schema("CCSPlayerController", "m_pInGameMoneyServices"),
-            money: schema(
-                "CCSPlayerController_InGameMoneyServices",
-                "m_iAccount"
-            ),
-            color: schema("CCSPlayerController", "m_iCompTeammateColor"),
-            name: schema("CBasePlayerController", "m_iszPlayerName"),
-            pawn: schema("CBasePlayerController", "m_hPawn"),
-            desired_fov: schema("CBasePlayerController", "m_iDesiredFOV"),
-            owner_entity: schema("C_BaseEntity", "m_hOwnerEntity"),
-            rank: schema("CCSPlayerController", "m_iCompetitiveRanking"),
-            rank_type: schema("CCSPlayerController", "m_iCompetitiveRankType"),
-            action_tracking_services: schema("CCSPlayerController", "m_pActionTrackingServices"),
+            // CS:GO entity offsets
+            steam_id: csgo_fixed(0x1E8),
+            money_services: csgo_fixed(0x1F0),
+            money: csgo_fixed(0x1F0),
+            color: csgo_fixed(0x204),
+            name: csgo_fixed(0x210),
+            pawn: csgo_fixed(0x1C8),
+            desired_fov: csgo_fixed(0x238),
+            owner_entity: csgo_fixed(0xF4),
+            rank: csgo_fixed(0x1324),
+            rank_type: csgo_fixed(0x1328),
+            action_tracking_services: csgo_fixed(0x1288),
         }
         entity: EntityOffsets {
-            health: schema("C_BaseEntity", "m_iHealth"),
-            max_health: schema("C_BaseEntity", "m_iMaxHealth"),
-            team: schema("C_BaseEntity", "m_iTeamNum"),
-            life_state: schema("C_BaseEntity", "m_lifeState"),
-            game_scene_node: schema("C_BaseEntity", "m_pGameSceneNode"),
-            velocity: schema("C_BaseEntity", "m_vecVelocity"),
-            collision: schema("C_BaseEntity", "m_pCollision"),
+            // CS:GO player entity offsets
+            health: csgo_fixed(0x100),
+            max_health: csgo_fixed(0x108),
+            team: csgo_fixed(0xF4),
+            life_state: csgo_fixed(0x25F),
+            game_scene_node: csgo_fixed(0x318),
+            velocity: csgo_fixed(0x114),
+            collision: csgo_fixed(0x310),
         }
         collision: CollisionOffsets {
-            mins: schema("CCollisionProperty", "m_vecMins"),
-            maxs: schema("CCollisionProperty", "m_vecMaxs"),
+            mins: csgo_fixed(0x8),
+            maxs: csgo_fixed(0x14),
         }
         pawn: PawnOffsets {
-            controller: schema("C_BasePlayerPawn", "m_hController"),
-            armor: schema("C_CSPlayerPawn", "m_ArmorValue"),
-            fov_multiplier: schema("C_BasePlayerPawn", "m_flFOVSensitivityAdjust"),
-            eye_offset: schema("C_BaseModelEntity", "m_vecViewOffset"),
-            shots_fired: schema("C_CSPlayerPawn", "m_iShotsFired"),
-            view_angles: schema("C_BasePlayerPawn", "v_angle"),
-            eye_angles: schema("C_CSPlayerPawn", "m_angEyeAngles"),
-            flags: schema("C_BaseEntity", "m_fFlags"),
-            crosshair_entity: schema("C_CSPlayerPawn", "m_iIDEntIndex"),
-            is_scoped: schema("C_CSPlayerPawn", "m_bIsScoped"),
-            deathmatch_immunity: schema("C_CSPlayerPawn", "m_bGunGameImmunity"),
-            observer_services: schema("C_BasePlayerPawn", "m_pObserverServices"),
-            spotted_state: schema("C_CSPlayerPawn", "m_entitySpottedState"),
-            flash_alpha: schema("C_CSPlayerPawnBase", "m_flFlashMaxAlpha"),
-            flash_duration: schema("C_CSPlayerPawnBase", "m_flFlashDuration"),
-            camera_services: schema("C_BasePlayerPawn", "m_pCameraServices"),
-            item_services: schema("C_BasePlayerPawn", "m_pItemServices"),
-            weapon_services: schema("C_BasePlayerPawn", "m_pWeaponServices"),
-            aim_punch_services: schema("C_CSPlayerPawn", "m_pAimPunchServices"),
-            bullet_services: schema("C_CSPlayerPawn", "m_pBulletServices"),
+            controller: csgo_fixed(0x1A0),
+            armor: csgo_fixed(0x104),
+            fov_multiplier: csgo_fixed(0x110),
+            eye_offset: csgo_fixed(0x10C),
+            shots_fired: csgo_fixed(0x29C),
+            view_angles: csgo_fixed(0x288),
+            eye_angles: csgo_fixed(0x290),
+            flags: csgo_fixed(0x100),
+            crosshair_entity: csgo_fixed(0x2A8),
+            is_scoped: csgo_fixed(0x2A0),
+            deathmatch_immunity: csgo_fixed(0x2A4),
+            observer_services: csgo_fixed(0x340),
+            spotted_state: csgo_fixed(0x350),
+            flash_alpha: csgo_fixed(0x20C),
+            flash_duration: csgo_fixed(0x210),
+            camera_services: csgo_fixed(0x330),
+            item_services: csgo_fixed(0x328),
+            weapon_services: csgo_fixed(0x320),
+            aim_punch_services: csgo_fixed(0x360),
+            bullet_services: csgo_fixed(0x370),
         }
         game_scene_node: GameSceneNodeOffsets {
-            dormant: schema("CGameSceneNode", "m_bDormant"),
-            origin: schema("CGameSceneNode", "m_vecAbsOrigin"),
-            node_to_world: schema("CGameSceneNode", "m_nodeToWorld"),
-            model_state: schema("CSkeletonInstance", "m_modelState"),
-            model_name: schema("CModelState", "m_ModelName"),
+            dormant: csgo_fixed(0xED),
+            origin: csgo_fixed(0x138),
+            node_to_world: csgo_fixed(0x30),
+            model_state: csgo_fixed(0x168),
+            model_name: csgo_fixed(0x8),
         }
         smoke: SmokeOffsets {
-            did_smoke_effect: schema("C_SmokeGrenadeProjectile", "m_bDidSmokeEffect"),
-            smoke_color: schema("C_SmokeGrenadeProjectile", "m_vSmokeColor"),
+            did_smoke_effect: csgo_fixed(0x3E8),
+            smoke_color: csgo_fixed(0x3F0),
         }
         molotov: MolotovOffsets {
-            is_incendiary: schema("C_MolotovProjectile", "m_bIsIncGrenade"),
+            is_incendiary: csgo_fixed(0x3E8),
         }
         inferno: InfernoOffsets {
-            is_burning: schema("C_Inferno", "m_bFireIsBurning"),
-            fire_count: schema("C_Inferno", "m_fireCount"),
-            fire_positions: schema("C_Inferno", "m_firePositions"),
+            is_burning: csgo_fixed(0x3E8),
+            fire_count: csgo_fixed(0x3F0),
+            fire_positions: csgo_fixed(0x3F8),
         }
         spotted_state: SpottedStateOffsets {
-            spotted: schema("EntitySpottedState_t", "m_bSpotted"),
-            mask: schema("EntitySpottedState_t", "m_bSpottedByMask"),
+            spotted: csgo_fixed(0x0),
+            mask: csgo_fixed(0x1),
         }
         camera_services: CameraServicesOffsets {
-            fov: schema("CCSPlayerBase_CameraServices", "m_iFOV"),
+            fov: csgo_fixed(0x0),
         }
         item_services: ItemServicesOffsets {
-            has_defuser: schema("CCSPlayer_ItemServices", "m_bHasDefuser"),
-            has_helmet: schema("CCSPlayer_ItemServices", "m_bHasHelmet"),
+            has_defuser: csgo_fixed(0x0),
+            has_helmet: csgo_fixed(0x4),
         }
         weapon_services: WeaponServicesOffsets {
-            weapons: schema("CPlayer_WeaponServices", "m_hMyWeapons"),
-            active_weapon: schema("CPlayer_WeaponServices", "m_hActiveWeapon"),
+            weapons: csgo_fixed(0x0),
+            active_weapon: csgo_fixed(0x4),
         }
         aim_punch_services: AimPunchServicesOffsets {
-            aim_punch_cache: schema_sub("CCSPlayer_AimPunchServices", "m_unpredictableBaseTick", 0x18),
+            aim_punch_cache: csgo_fixed(0x0),
         }
         action_tracking: ActionTrackingServicesOffsets {
-            round_kills: schema("CCSPlayerController_ActionTrackingServices", "m_iNumRoundKills"),
-            round_damage: schema("CCSPlayerController_ActionTrackingServices", "m_flTotalRoundDamageDealt"),
-            per_round_stats: schema("CCSPlayerController_ActionTrackingServices", "m_perRoundStats"),
+            round_kills: csgo_fixed(0x0),
+            round_damage: csgo_fixed(0x4),
+            per_round_stats: csgo_fixed(0x8),
         }
         bullet_services: BulletServicesOffsets {
-            total_hits: schema("CCSPlayer_BulletServices", "m_totalHitsOnServer"),
+            total_hits: csgo_fixed(0x0),
         }
         per_round_stats: PerRoundStatsOffsets {
-            kills: schema("CSPerRoundStats_t", "m_iKills"),
-            deaths: schema("CSPerRoundStats_t", "m_iDeaths"),
-            assists: schema("CSPerRoundStats_t", "m_iAssists"),
-            damage: schema("CSPerRoundStats_t", "m_iDamage"),
-            size: class_size("CSPerRoundStats_t"),
+            kills: csgo_fixed(0x0),
+            deaths: csgo_fixed(0x4),
+            assists: csgo_fixed(0x8),
+            damage: csgo_fixed(0xC),
+            size: csgo_fixed(0x20),
         }
         observer_services: ObserverServicesOffsets {
-            target: schema("CPlayer_ObserverServices", "m_hObserverTarget"),
+            target: csgo_fixed(0x0),
         }
         econ_item_view: EconItemViewOffsets {
-            item_definition_index: schema("C_EconItemView", "m_iItemDefinitionIndex"),
+            item_definition_index: csgo_fixed(0x0),
         }
         weapon: WeaponOffsets {
-            attribute_manager: schema("C_EconEntity", "m_AttributeManager"),
-            item: schema("C_AttributeContainer", "m_Item"),
-            item_definition_index: schema("C_EconItemView", "m_iItemDefinitionIndex"),
-            clip_primary: schema("C_BasePlayerWeapon", "m_iClip1"),
-            reserve_ammo: schema("C_BasePlayerWeapon", "m_pReserveAmmo"),
+            attribute_manager: csgo_fixed(0x0),
+            item: csgo_fixed(0x0),
+            item_definition_index: csgo_fixed(0x0),
+            clip_primary: csgo_fixed(0x0),
+            reserve_ammo: csgo_fixed(0x0),
         }
         planted_c4: PlantedC4Offsets {
-            is_ticking: schema("C_PlantedC4", "m_bBombTicking"),
-            blow_time: schema("C_PlantedC4", "m_flC4Blow"),
-            being_defused: schema("C_PlantedC4", "m_bBeingDefused"),
-            is_defused: schema("C_PlantedC4", "m_bBombDefused"),
-            has_exploded: schema("C_PlantedC4", "m_bHasExploded"),
-            defuse_time: schema("C_PlantedC4", "m_flDefuseCountDown"),
-            defuse_time_left: schema("C_PlantedC4", "m_flDefuseCountDown"),
+            is_ticking: csgo_fixed(0x3E8),
+            blow_time: csgo_fixed(0x3F0),
+            being_defused: csgo_fixed(0x3F8),
+            is_defused: csgo_fixed(0x3FC),
+            has_exploded: csgo_fixed(0x400),
+            defuse_time: csgo_fixed(0x404),
+            defuse_time_left: csgo_fixed(0x408),
         }
         model_state: ModelState {
-            skeleton_instance: schema("CBodyComponentSkeletonInstance", "m_skeletonInstance"),
+            skeleton_instance: csgo_fixed(0x0),
         }
         network_velocity: NetworkVelocityOffsets {
             x: fixed(0x10),
@@ -322,22 +327,22 @@ schema! {
             z: fixed(0x20),
         }
         entity_identity: EntityIdentityOffsets {
-            size: class_size("CEntityIdentity"),
+            size: csgo_fixed(0x50),
         }
     }
 
     physics: {
         hull: PhysHullOffsets {
-            vertices: phys("RnHull_t", "m_VertexPositions"),
-            edges: phys("RnHull_t", "m_Edges"),
-            faces: phys("RnHull_t", "m_Faces"),
-            flags: phys("RnHull_t", "m_nFlags"),
+            vertices: csgo_fixed(0x0),
+            edges: csgo_fixed(0x8),
+            faces: csgo_fixed(0x10),
+            flags: csgo_fixed(0x18),
         }
         mesh: PhysMeshOffsets {
-            vertices: phys("RnMesh_t", "m_Vertices"),
-            triangles: phys("RnMesh_t", "m_Triangles"),
-            materials: phys("RnMesh_t", "m_Materials"),
-            flags: phys("RnMesh_t", "m_nFlags"),
+            vertices: csgo_fixed(0x0),
+            triangles: csgo_fixed(0x8),
+            materials: csgo_fixed(0x10),
+            flags: csgo_fixed(0x18),
         }
     }
 }
